c99f04b65d61adb621f29556cee03c3b54a7cce4
cheikou <cheikou.s99@gmail.com>
2020-11-02 14:25:38 +0100

    Modification du type de l'attribut date dans Commit.java et CommitBuilder.java en LocalDateTime.

--- gitrawdata/src/main/java/up/visulog/gitrawdata/Commit.java
+++ gitrawdata/src/main/java/up/visulog/gitrawdata/Commit.java
@@ -5,19 +5,23 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+
 public class Commit {
     // FIXME: (some of) these fields could have more specialized types than String
     public final String id;
-    public final String date;
+    public final LocalDateTime date;
     public final String author;
     public final String description;
     public final String mergedFrom;
 
-    public Commit(String id, String author, String date, String description, String mergedFrom) {
+    public Commit(String id, String author, LocalDateTime date, String description, String mergedFrom) {
         this.id = id;
         this.author = author;
         this.date = date;
@@ -26,11 +30,14 @@ public class Commit {
     }
 
     // TODO: factor this out (similar code will have to be used for all git commands)
+    //renvoie la liste des commits donnés par la commande "git log" exécutéé depuis gitPath
     public static List<Commit> parseLogFromCommand(Path gitPath) {
+    	//builder: commande "git log" qui va être exécutée depuis le dossier gitPath
         ProcessBuilder builder =
                 new ProcessBuilder("git", "log").directory(gitPath.toFile());
         Process process;
         try {
+        	//exécution de builder
             process = builder.start();
         } catch (IOException e) {
             throw new RuntimeException("Error running \"git log\".", e);
@@ -39,7 +46,7 @@ public class Commit {
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
         return parseLog(reader);
     }
-
+    
     public static List<Commit> parseLog(BufferedReader reader) {
         var result = new ArrayList<Commit>();
         Optional<Commit> commit = parseCommit(reader);
@@ -54,6 +61,7 @@ public class Commit {
      * Parses a log item and outputs a commit object. Exceptions will be thrown in case the input does not have the proper format.
      * Returns an empty optional if there is nothing to parse anymore.
      */
+    //crée des objet de la class commit à partir de la sortie de la commande git log
     public static Optional<Commit> parseCommit(BufferedReader input) {
         try {
 
@@ -78,7 +86,8 @@ public class Commit {
                     case "Date":
                         builder.setDate(fieldContent);
                         break;
-                    default: // TODO: warn the user that some field was ignored
+                    default: 
+                    	System.out.println("Le champ " + fieldName + " a été ignoré");
                 }
                 line = input.readLine(); //prepare next iteration
                 if (line == null) parseError(); // end of stream is not supposed to happen now (commit data incomplete)
@@ -108,7 +117,7 @@ public class Commit {
         return "Commit{" +
                 "id='" + id + '\'' +
                 (mergedFrom != null ? ("mergedFrom...='" + mergedFrom + '\'') : "") + //TODO: find out if this is the only optional field
-                ", date='" + date + '\'' +
+                ", date='" + date.format(DateTimeFormatter.ofPattern("EEE LLL d HH:mm:ss yyyy", java.util.Locale.ENGLISH)) + '\'' +
                 ", author='" + author + '\'' +
                 ", description='" + description + '\'' +
                 '}';
