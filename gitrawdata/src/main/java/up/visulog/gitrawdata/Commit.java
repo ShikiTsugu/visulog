a41969bdfed3dae8946e832a27de744304aa3445
cheikou <cheikou.s99@gmail.com>
2020-11-09 22:27:42 +0100

    Ajout de méthodes dans Commit.java pour l'analyse du nombre de commits

--- gitrawdata/src/main/java/up/visulog/gitrawdata/Commit.java
+++ gitrawdata/src/main/java/up/visulog/gitrawdata/Commit.java
@@ -5,11 +5,17 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalField;
+import java.time.temporal.WeekFields;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 
 
@@ -111,7 +117,70 @@ public class Commit {
     private static void parseError() {
         throw new RuntimeException("Wrong commit format.");
     }
-
+    
+    // Renvoie une HashMap contenant dont le clé sont les jours et les valeurs, le nombre de commits effectués
+    public static HashMap numberOfCommitsPerDay(Path gitPath) {
+    	List<Commit> commits = parseLogFromCommand(gitPath);
+    	HashMap<LocalDate, Integer> m = new HashMap<LocalDate, Integer>();
+    	for(Commit c : commits) {
+    		if(m.get(c.date.toLocalDate())==null) {
+    			int valeur = 0;
+        		m.put(c.date.toLocalDate(),1 + valeur);
+    		}else {
+	    		int valeur = (int) m.get(c.date.toLocalDate());
+	    		m.put(c.date.toLocalDate(),1 + valeur);
+    		}
+    	}
+    	return m;
+    }
+    
+    //Renvoie le nombre de commits des n derniers jours
+    public static int numberOfCommits (int n, Path gitPath) {
+    	LocalDate day = LocalDate.now().minusDays(n);
+    	int number = 0;
+    	HashMap<LocalDate, Integer> commits = numberOfCommitsPerDay(gitPath);
+    	for(HashMap.Entry<LocalDate, Integer> m : commits.entrySet()) {
+    		if(m.getKey().isAfter(day)) {
+    			number += m.getValue();
+    		}
+    	}
+    	return number;
+    }
+    
+    //Renvoie le nombre de commit fait en moyenne les n derniers jours
+    public static double averageNumberOfCommits(int n, Path gitPath) {
+    	return (double) numberOfCommits(n,gitPath)/(double) n;
+    }
+    
+    public static double ecartype(int n, Path gitPath) {
+    	double average = averageNumberOfCommits(n,gitPath);
+    	LocalDate day = LocalDate.now().minusDays(n);
+    	int sum = 0;
+    	HashMap<LocalDate, Integer> commits = numberOfCommitsPerDay(gitPath);
+    	for(HashMap.Entry<LocalDate, Integer> m : commits.entrySet()) {
+    		if(m.getKey().isAfter(day)) {
+    			sum += (m.getValue() - average)*(m.getValue() - average);
+    		}
+    	}
+    	return (double) sum/(double) numberOfCommits (n,gitPath);
+    }
+    
+    //Renvoie le premier jour de la semaine de l'argument date
+    public static LocalDate firstDayOfWeek(LocalDate date) {
+    	TemporalField fieldFR = WeekFields.of(Locale.FRANCE).dayOfWeek();
+    	return date.with(fieldFR, 1);
+    }
+    
+    //renvoie le nombre de commits effectués depuis le 15 Septembre. 
+    public static int totalNumberOfCommits(Path gitPath) {
+    	return numberOfCommits((int) LocalDate.parse("2020-09-15").until(LocalDate.now(), ChronoUnit.DAYS), gitPath);
+    }
+    
+    //Renvoie le nombre de commit de la semaine courante
+    public static int numberOfCommitsInTheCurrentWeek(Path gitPath) {
+    	return numberOfCommits((int) firstDayOfWeek(LocalDate.now()).until(LocalDate.now(), ChronoUnit.DAYS), gitPath);
+    }
+    
     @Override
     public String toString() {
         return "Commit{" +
