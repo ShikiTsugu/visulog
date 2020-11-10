7f5689d296d6a645b26772ee086433b9151960f3
HOU <wtfomg@live.fr>
2020-11-10 15:35:17 +0100

    Classe PluginConfig définie

--- config/src/main/java/up/visulog/config/PluginConfig.java
+++ config/src/main/java/up/visulog/config/PluginConfig.java
@@ -1,5 +1,25 @@
 package up.visulog.config;
+import java.util.Map;
+import java.util.HashMap;
 
-// TODO: define what this type should be (probably a Map: settingKey -> settingValue)
-public interface PluginConfig {
+public class PluginConfig{
+	private Map<String, String> plugins=new HashMap<String, String>();
+	
+	public PluginConfig(Map<String, String> p) {
+		plugins = p;
+	}
+	
+	public Map<String, String> getMap(){
+		return plugins;
+	}
+	
+	public String[] getKeys() {
+		String[]keys=new String[plugins.size()];
+		for (String s: plugins.keySet()) {
+			for (int i=0; i<keys.length; i++) {
+				keys[i]=s;
+			}
+		}
+		return keys;
+	}
 }
